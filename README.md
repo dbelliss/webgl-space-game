030dcfa8ac362fe016a5257cc6ee307855b4fffc
Davey <daveybelliss@gmail.com>
2019-02-26 00:13:27 -0800

    Alpha version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# webgl-space-game
-A game created in WebGL
+# WebGL Practice
+
+A repository to track learnign progress on WebGL
+
+## Tools
+http://glmatrix.net/
+https://github.com/madrobby/keymaster
+
+## References:
+* http://learnwebgl.brown37.net/index.html
+* https://www.youtube.com/watch?v=kB0ZVUrI4Aw
+* https://github.com/acgessler/assimp2json
+
+## Assets
+* https://opengameart.org/content/simple-toon-wooden-crate-texture
+* https://www.turbosquid.com/FullPreview/Index.cfm/ID/1051001
+* https://soundcloud.com/rin7ishi/rin-ishi-freeform
+* https://www.turbosquid.com/3d-models/free-c4d-model-retro-rocket/687319
+* https://freesound.org/people/TreasureSounds/sounds/332629/
+* https://freesound.org/people/qubodup/sounds/151624/
---
+++ game.js
@@ -0,0 +1,239 @@
+"use strict";
+
+function Initialize() {
+
+    loadJSONResource('./Assets/Asteroid.json', function (modelErr, asteroidObj) {
+        loadJSONResource('./Assets/rocket.json', function (modelErr, rocketObj) {
+            var game = new Game(asteroidObj, rocketObj);
+        });
+    });
+}
+
+class Game {
+    initializeMatrices() {
+        this.worldMatrix = new Float32Array(16);
+        this.viewMatrix = new Float32Array(16);
+        this.projMatrix = new Float32Array(16);
+    }
+
+    constructor(asteroidJson, rocketJson) {
+        var music = document.getElementById("music");
+        var pickup = document.getElementById("pickup");
+        var crash = document.getElementById("crash");
+        var playPromise = music.play()
+
+        if (playPromise !== null){
+            playPromise.catch(() => {
+                console.error("Could not payback music");
+            })
+        }
+
+        // Create WebGL object
+        var canvas = document.getElementById('game-surface');
+        this.canvas = canvas;
+        this.gl = InitializeGL(canvas);
+        if (this.gl == undefined) {
+            console.error('Could not initialize WebGL');
+            return;
+        }
+        var gl = this.gl  // Shorter name
+        clearGL(gl)
+
+        // Initialize world, view, and proj matrixes
+        this.initializeMatrices();
+
+        // Load all textures
+        this.textureLoader = new TextureLoader(gl)
+
+        // Load the program that WebGL will use
+        this.textureProgram = new TextureProgram(gl, this.worldMatrix, this.viewMatrix, this.projMatrix, this.canvas.clientWidth / this.canvas.clientHeight)
+
+        // Load renderer
+        this.renderer = new Renderer(this.gl, this.textureLoader, this.textureProgram);
+
+        this.gl.useProgram(this.textureProgram.program);
+
+        // Create list of all active GameObjects to act on during FixedUpdate
+        this.activeGameObjects = []
+
+        // Create GameObjects
+        this.crates = []
+        this.asteroids = []
+        this.createPlayer(rocketJson);
+        this.createAsteroids(1000, asteroidJson)
+        this.createCrates(1000)
+        this.createWalls()
+        this.camera = new Camera(gl, this.worldMatrix, this.viewMatrix, this.projMatrix);
+
+        // Render Loop
+        var numFrames = 0
+        var theta = 0
+        var phi = 0
+        function render () {
+            clearGL(this.gl)
+            var cameraInput = InputManager.readCameraInput()
+
+            theta = cameraInput.x;
+            phi = cameraInput.y;
+            this.camera.trackObject(this.player, theta, phi);
+            this.textureProgram.updateCamera()
+
+            if (this.asteroids.length > 0) {
+                var renderDataList = this.asteroids[0].renderData
+                var renderData = renderDataList[0]
+                this.textureProgram.batchDraw(this.asteroids, renderData.vertices, renderData.indices, renderData.textureIndices, renderData.texture, gl.DYNAMIC_DRAW)
+            }
+
+            if (this.crates.length > 0) {
+                var renderDataList = this.crates[0].renderData
+                var renderData = renderDataList[0]
+                this.textureProgram.batchDraw(this.crates, renderData.vertices, renderData.indices, renderData.textureIndices, renderData.texture, gl.DYNAMIC_DRAW)
+            }
+
+            if (this.player) {
+                var gameObject = this.player
+                var renderDataList = gameObject.renderData
+                for (var j = 0; j < renderDataList.length; j++) {
+                    var renderData = renderDataList[j]
+                    this.textureProgram.drawMesh(renderData.vertices, renderData.indices, renderData.textureIndices, renderData.texture, gl.DYNAMIC_DRAW, gameObject.transform)
+                }
+            }
+
+            numFrames++;
+            requestAnimationFrame(render.bind(this));
+        };
+        requestAnimationFrame(render.bind(this));
+        this.beginFixedUpdateLoop();
+    }
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async beginFixedUpdateLoop() {
+        var prevTime = performance.now() / 1000; // Get seconds
+        var curTime = performance.now() / 1000;
+        var deltaTime = 0
+        var updateNum = 0
+        while(true) {
+            curTime = performance.now() / 1000;
+            deltaTime = curTime - prevTime;
+            prevTime = curTime;
+            this.activeGameObjects.forEach(function (gameObject) {
+                gameObject.fixedUpdate(.02)
+            })
+
+            for (var i = 0; i < this.crates.length; i++) {
+                var crate = this.crates[i];
+                if (crate.transform.position.distance(this.player.transform.position) < 3) {
+                    console.log("Crate!")
+                    var pickupPromise = pickup.play()
+                    if (pickupPromise !== null){
+                        pickupPromise.catch(() => {
+                            console.error("Could not playback pickup");
+                        })
+                    }
+                    this.crates.splice(i,1);
+                }
+                if (i < this.crates.length/3) {
+                    crate.transform.rotation.x = (crate.transform.rotation.x + 1) % 360
+                }
+                else if (i < this.crates.length * 2/3) {
+                    crate.transform.rotation.y = (crate.transform.rotation.y + 1) % 360
+                }
+                else{
+                    crate.transform.rotation.z = (crate.transform.rotation.z + 1) % 360
+                }
+            }
+            for (var i = 0; i < this.asteroids.length; i++) {
+                var asteroid = this.asteroids[i]
+                if (asteroid.transform.position.distance(this.player.transform.position) < 5) {
+                    var crashPromise = crash.play()
+                    if (crashPromise !== null){
+                        crashPromise.catch(() => {
+                            console.error("Could not playback crash");
+                        })
+                    }
+                    console.log("Hit by asteroid")
+                }
+                if (i < this.asteroids.length/3) {
+                    asteroid.transform.rotation.x = (this.asteroids[i].transform.rotation.x + this.asteroids[i].rotationSpeed) % 360
+                }
+                else if (i < this.asteroids.length * 2/3) {
+                    asteroid.transform.rotation.y = (this.asteroids[i].transform.rotation.y + this.asteroids[i].rotationSpeed) % 360
+                }
+                else{
+                    asteroid.transform.rotation.z = (this.asteroids[i].transform.rotation.z + this.asteroids[i].rotationSpeed) % 360
+                }
+            }
+
+            updateNum++;
+            await this.sleep(1000/60);
+        }
+    }
+
+    createAsteroids(numAsteroids, asteroidJson) {
+        console.log("Creating asteroids");
+        var scales = [new Vector3(.1, .1, .1), new Vector3(.5, .2, .4), new Vector3(.1, .3, .2), new Vector3(.5, .5, .5), new Vector3(1, 1, 1)]
+        var renderData = []
+        var texture = this.textureLoader.getTexture("rock")
+        for (var i = 0; i < asteroidJson.meshes.length; i++) {
+            var vertices = asteroidJson.meshes[i].vertices
+            var indices = [].concat.apply([], asteroidJson.meshes[i].faces)
+            var textureVertices = asteroidJson.meshes[i].texturecoords[0]
+            renderData.push(new RenderData(texture, vertices, indices, textureVertices))
+        }
+
+        for (var i = 0; i < numAsteroids; i++) {
+            var asteroid = new MeshObject("asteroid" + i, Vector3.random(-700, 700), renderData)
+            asteroid.transform.scale = scales[Math.floor(scales.length * Math.random())]
+            asteroid.transform.rotation.x = Math.random() * 360
+            asteroid.transform.rotation.y = Math.random() * 360
+            asteroid.transform.rotation.z = Math.random() * 360
+            asteroid.rotationSpeed = Math.random() * 2
+            this.asteroids.push(asteroid);
+            this.addGameObject(asteroid);
+        }
+    }
+
+    createPlayer(rocketJson) {
+        console.log("Creating player");
+        var renderData = []
+        var texture = this.textureLoader.getTexture("rocket")
+        for (var i = 0; i < rocketJson.meshes.length; i++) {
+            var vertices = rocketJson.meshes[i].vertices
+            var indices = [].concat.apply([], rocketJson.meshes[i].faces)
+            var textureVertices = rocketJson.meshes[i].texturecoords[0]
+            renderData.push(new RenderData(texture, vertices, indices, textureVertices))
+        }
+        this.player = new Player("Player", new Vector3(0,0,0), renderData);
+        this.player.transform.scale.scale(.01)
+        this.addGameObject(this.player);
+    }
+
+    createCrates(numCrates) {
+        console.log("Creating crates");
+        for (var i = 0; i < numCrates; i++) {
+            var crate = new Cube("crate" + i, Vector3.random(-500, 500), this.textureLoader.getTexture("crate"))
+            crate.transform.rotation.x = Math.random() * 360
+            crate.transform.rotation.y = Math.random() * 360
+            crate.transform.rotation.z = Math.random() * 360
+            this.crates.push(crate);
+            this.addGameObject(crate);
+        }
+
+        var crate = new Cube("crate" + i, Vector3.random(-500, 500), this.textureLoader.getTexture("crate"))
+        crate.transform.position = new Vector3(5,0,0)
+                    this.crates.push(crate);
+            this.addGameObject(crate);
+    }
+
+    addGameObject(gameObject) {
+        this.activeGameObjects.push(gameObject);
+//        this.renderer.addObject(gameObject)
+    }
+
+    createWalls() {
+        console.log("Walls have not yet been implemented");
+    }
+}
