25a1f3df33dbd2635c5fa0ca15cd5f9cc28f5839
Davey <daveybelliss@gmail.com>
2019-03-04 17:04:11 -0800

    Player and Camera movement, correctly uses quaternions and rotates on the local axis

--- Lib/transform.js
+++ Lib/transform.js
@@ -8,7 +8,7 @@ class Transform {
     constructor(position) {
         this.position = position
         this.scale = new Vector3(1, 1, 1);
-        this.rotation = glMatrix.quat.fromValues(0, 0, 0, 1);
+        this.rotation = glMatrix.quat.fromValues(0, 0, 0, 1); // Identity Quat
         this.children = [];
         this.parentTransform = undefined;
     }
